5430e6095ecdf6e7dd3e3dfafcec10772a92a842
Cesar Cordoba <cordobacesar29@gmail.com>
2024-01-19 13:52:22 -0300

    pages and routes

---
+++ src/app/dashboard/clients/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>client page</div>
+  )
+}
+
+export default HomePage
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import{ Container} from '@mui/material'
+
+function Dashboard() {
+  return (
+    <Container>dashboard</Container>
+  )
+}
+
+export default Dashboard
