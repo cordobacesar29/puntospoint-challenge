faf5ea0371d5b5e1c09475a27f24b6d7dba251c2
Cesar Cordoba <cordobacesar29@gmail.com>
2024-01-23 17:39:47 -0300

    refactor

--- src/app/api/dataAPI.ts
+++ src/app/api/dataAPI.ts
@@ -1,40 +1,11 @@
+import { InputType } from "@/interfaces/input.type";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: 'http://localhost:3002'
 })
 
-export const getDataToday = async () => {
-  const res = await api.get('/today')
-  return res.data
-}
-
-export const getDataByWeek = async () => {
-  const res = await api.get('/week')
-  return res.data
-}
-export const getDataByThisMonth = async () => {
-  const res = await api.get('/this_month')
-  return res.data
-}
-
-export const getDataBySixMont = async () => {
-  const res = await api.get('/six_months')
-  return res.data
-}
-export const getDataByYTD = async () => {
-  const res = await api.get('/year_to_date')
-  return res.data
-}
-export const getDataByOneYear = async () => {
-  const res = await api.get('/one_year')
-  return res.data
-}
-export const getDataMax = async () => {
-  const res = await api.get('/max')
-  return res.data
-}
-export const getDataPersonalized = async () => {
-  const res = await api.get('/personalized')
+export const getData = async (props: InputType) => {
+  const res = await api.get(`/${props}`)
   return res.data
 }
