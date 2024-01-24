c08c33b93d1a67a04e4010c62aa8a3706cbad980
Cesar Cordoba <cordobacesar29@gmail.com>
2024-01-24 11:07:26 -0300

    fxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
-import { Box, Button, Typography } from '@mui/material'
-import Link from 'next/link'
-import React from 'react'
-import { Colors } from './utils/Colors'
-import { ROUTES } from './utils/routes'
+import { Box, Button, Typography } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+import { ROUTES } from "./utils/routes";
 
 function HomePage() {
   return (
-    <Box display={'flex'} flexDirection={'column'} width={'100%'} height={'100vh'} alignItems={'center'} justifyContent={'center'}>
-      <Typography>
-        Bienvenidos a mi proyecto. 👨‍💻
-      </Typography>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      width={"100%"}
+      height={"100vh"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Typography>Bienvenidos a mi proyecto. 👨‍💻</Typography>
       <Link href={ROUTES.DASHBOARD}>
         <Button>Ir al dashboard</Button>
       </Link>
     </Box>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
